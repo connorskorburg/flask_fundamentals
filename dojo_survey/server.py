b65d0836269e53d35dbb0f89e5361c7b4cba690f
Connor <connorskorburg@gmail.com>
2020-07-19 18:54:08 -0500

    updated session in dojo survery practice

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+
+app.secret_key = 'dojo_survey_key'
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/show')
+def show():
+    return render_template("results.html")
+
 @app.route('/createNinja', methods=['POST'])
 def create_user():
     print(request.form)
-    name = request.form['name']
-    location = request.form['location']
-    language = request.form['language']
-    comment = request.form['comment']
-
-    return render_template("results.html", name=name, location=location, language=language, comment=comment)
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect('/show')
 
 if __name__=="__main__":
     app.run(debug=True)
