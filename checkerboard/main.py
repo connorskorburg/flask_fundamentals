ab7990c2d8b14be31b2717be3f21c273e1abfd05
Connor <connorskorburg@gmail.com>
2020-07-19 16:58:50 -0500

    completed checkerboard practice

--- checkerboard/main.py
+++ checkerboard/main.py
@@ -5,11 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/<int:num>')
-def checkers(num):
-    return render_template("index.html", num=num)
+@app.route('/<int:rows>')
+def checkerRow(rows):
+    return render_template("index.html", rows=rows, columns=1)
 
+@app.route('/<int:rows>/<int:columns>')
+def checkerRowColumn(rows, columns):
+    return render_template("index.html", rows=rows, columns=columns)
 
+@app.route('/<int:rows>/<int:columns>/<color1>')
+def checkerColorOne(rows, columns, color1):
+    return render_template("index.html", rows=rows, columns=columns, color1=color1)
+
+@app.route('/<int:rows>/<int:columns>/<color1>/<color2>')
+def checkerColorTwo(rows, columns, color1, color2):
+    return render_template("index.html", rows=rows, columns=columns, color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
